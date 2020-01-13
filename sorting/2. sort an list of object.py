865db8ea852bcf614829e6df6bb94b626bd4c019
Nguyen Huu Hieu <hieunh1801@gmail.com>
2020-01-13 23:39:20 +0700

    sort on multiple file

--- sorting/2. sort an list of object.py
+++ sorting/2. sort an list of object.py
@@ -5,7 +5,6 @@ from pprint import pprint
     2. Sort trên multiple attribute using lambda
 """
 
-# 1. Sort on one attribute
 list_of_people = [
     {"name": "Hieu 1", "age": 22},
     {"name": "Hieu 2", "age": 23},
@@ -19,9 +18,12 @@ list_of_people = [
     {"name": "Hieu 2", "age": 20},
     {"name": "Hieu 1", "age": 20},
 ]
-
+# 1. Sort on one attribute
 sorted_list = sorted(list_of_people, key=lambda people: people["age"])
 sorted_list = sorted(list_of_people, key=lambda people: people["name"])
+
+# 2. Sort multiple attribute
 sorted_list = sorted(list_of_people,
                      key=lambda people: (people["age"], people["name"]))
+# đảo ngược tuổi  => -people["age"]
 pprint(sorted_list)
